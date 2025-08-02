1bde588653b69ac39c402ffe539c6ea56477ab5c
RidwanRosling <ridwansbdt@gmail.com>
2025-08-02 17:12:40 +0700

    add templating engine

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const adminRoutes = require("./routers/admin");
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routers/admin");
 const shopRoutes = require("./routers/shop");
 // const bodyParser = require("body-parser");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use("/", (req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.render("404");
 });
 // const server = http.createServer(app);
 
--- routers/shop.js
+++ routers/shop.js
@@ -2,13 +2,23 @@ const express = require("express");
 const path = require("path");
 // const app = express();
 const router = express.Router();
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 // app.get("/", (req, res, next) => {
 //   console.log("In another middleware");
 //   res.send("<h1>Hello from Express!</h1>");
 // });
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  // console.log("shop", adminData.products);
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // app.render("shop");
+
+  const products = adminData.products;
+
+  // res.render() selalu menerima 2 argumen -> 1. Nama file view (tanpa ekstensi) & 2. Data yang akan dikirim ke view tersebut
+
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
