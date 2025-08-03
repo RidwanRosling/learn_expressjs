3e310051c127316a96ac866d2cf1aaa7a79f59f2
RidwanRosling <ridwansbdt@gmail.com>
2025-08-03 20:26:59 +0700

    change pug to ejs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routers/admin");
@@ -19,7 +19,7 @@ app.use(shopRoutes);
 
 app.use("/", (req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.render("404", { pageTitle: "Page Not Found" });
+  res.render("404", { pageTitle: "Page Not Found", path: null });
 });
 // const server = http.createServer(app);
 
--- routers/admin.js
+++ routers/admin.js
@@ -14,7 +14,7 @@ router.get("/add-product", (req, res, next) => {
 
   res.render("add-product", {
     pageTitle: "Add Product",
-    path: "/admin/add-product",
+    path: "/admin/add-product"
   });
 });
 
